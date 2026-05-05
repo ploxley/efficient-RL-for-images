5664fbc560268db67f8135584144882fe4fc8aea
Peter Loxley <193450474+ploxley@users.noreply.github.com>
2026-05-05 13:04:19 +1000

    Update paper_figures.m

--- paper_figures.m
+++ paper_figures.m
@@ -2,6 +2,24 @@
 
 % Uncomment and run each code block as necessary
 
+% % Figure 3 (number_of_images = 1 instead of 48)
+% clear all
+% number_of_images = 1; % 48;
+% filename_vec = {'ImGDS_*.txt', 'OC_up*.txt', 'W*.txt', 'OC_S*.txt'};
+% max_rep_ave_iters = 4e3;
+% ar = arrayfun(@(x) 10*x, [2,4,8,16,32,64,128,256]);
+% legend_contents = ["\times 1 Raw Image","\times 4 Raw Image","\times 1 Whitened Image","\times 4 Sparse Code"];
+% run("figure_driver_2")
+
+% % Figure 4 (number_of_images = 1 takes around 40 minutes with a gpu)
+% clear all
+% number_of_images = 1; % 48;
+% filename_vec = {'W*.txt', 'OC_S*.txt', 'OOC_S*.txt', 'OOOC_S*.txt'};
+% max_rep_ave_iters = 10e3;
+% ar = arrayfun(@(x) 100*x, [1,2,4,8,16,32,64,128,256]);
+% legend_contents = ["","\times 4 Sparse Code","\times 16 Sparse Code","\times 64 Sparse Code"];
+% run("figure_driver_2")
+
 % % Figure 5
 % clear all
 % initial_state_opt = {[0,1],[0,0]};
@@ -23,24 +41,6 @@
 % initial_state_opt = {[4,0],[0,0]};
 % initial_state_greedy = initial_state_opt;
 % run("figure_driver_1")
-
-% % Figure 3 (number_of_images = 1 instead of 48)
-% clear all
-% number_of_images = 1; % 48;
-% filename_vec = {'ImGDS_*.txt', 'OC_up*.txt', 'W*.txt', 'OC_S*.txt'};
-% max_rep_ave_iters = 4e3;
-% ar = arrayfun(@(x) 10*x, [2,4,8,16,32,64,128,256]);
-% legend_contents = ["\times 1 Raw Image","\times 4 Raw Image","\times 1 Whitened Image","\times 4 Sparse Code"];
-% run("figure_driver_2")
-
-% % Figure 4 (number_of_images = 1 takes around 40 minutes with a gpu)
-% clear all
-% number_of_images = 1; % 48;
-% filename_vec = {'W*.txt', 'OC_S*.txt', 'OOC_S*.txt', 'OOOC_S*.txt'};
-% max_rep_ave_iters = 10e3;
-% ar = arrayfun(@(x) 100*x, [1,2,4,8,16,32,64,128,256]);
-% legend_contents = ["","\times 4 Sparse Code","\times 16 Sparse Code","\times 64 Sparse Code"];
-% run("figure_driver_2")
 
 % % Figure 7 (takes around 5 to 6 hours with a gpu)
 % clear all
